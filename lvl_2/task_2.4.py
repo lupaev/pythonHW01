56586276388791acac508c8f4048940a8df00be5
sergej <lupaevsv@gmail.com>
2023-04-24 15:23:05 +0700

    task 2.4

---
+++ lvl_2/task_2.4.py
@@ -0,0 +1,53 @@
+# Задача 2.4.
+
+# Пункт A.
+# Напишите функцию, которая удаляет все восклицательные знаки из заданной строк.
+# Например,
+# foo("Hi! Hello!") -> "Hi Hello"
+# foo("") -> ""
+# foo("Oh, no!!!") -> "Oh, no"
+
+def remove_exclamation_marks(s):
+    return print(s.replace("!", ""))
+
+
+# Пункт B.
+# Удалите восклицательный знак из конца строки. 
+# remove("Hi!") == "Hi"
+# remove("Hi!!!") == "Hi!!"
+# remove("!Hi") == "!Hi"
+
+def remove_last_em(s):
+    if s.endswith('!'):
+        return print(s[:-1])
+    else:
+        return print(s)
+
+
+
+# Дополнительно
+
+# Пункт С.
+# Удалите слова из предложения, если они содержат ровно один восклицательный знак.
+# Слова разделены одним пробелом.
+# Например,
+# remove("Hi!") === ""
+# remove("Hi! Hi!") === ""
+# remove("Hi! Hi! Hi!") === ""
+# remove("Hi Hi! Hi!") === "Hi"
+# remove("Hi! !Hi Hi!") === ""
+# remove("Hi! Hi!! Hi!") === "Hi!!"
+# remove("Hi! !Hi! Hi!") === "!Hi!"
+
+def remove_word_with_one_em(s):
+    list_s = s.split(' ')
+    new_s = ""
+    for el in list_s:
+        if el.startswith('!') and el.endswith('!'):
+            new_s += el + ' '
+        elif not el.startswith('!') and not el.endswith('!'):
+            new_s += el + ' '
+        elif el.find('!', 1, -1) > -1:
+            new_s += el + ' '
+
+    print(new_s)
